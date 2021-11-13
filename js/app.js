b4bfdcceddde910e445fc0358bc0b8ed48fdb587
zac234 <zacbenincasa@gamil.com>
2021-11-13 16:10:28 -0700

    added another js page for the slected page

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var red_button = document.getElementById("red_button")
+var Charizard = document.getElementById("charizard")
+var blue_button = document.getElementById("blue_button")
+
+Cookie.set("red_button_pushed", "yes")
+Cookie.set("Charizard_pushed", "yes")
+Cookie.set("blue_button_pushed", "yes")
+
+
